9287e3d488d3711f435df9672ef1f66d34f4e432
AnkushP <ankushpokalwar777@gmail.com>
2020-03-08 21:00:33 +0530

    login view and permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from profiles_api import permissions
 from profiles_api import serializers
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -51,6 +57,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
         """Test API ViewSet"""
 
+        serializer_class = serializers.HelloSerializer
+
         def list(self, request):
             """Return a hello message."""
 
@@ -60,3 +68,67 @@ class HelloViewSet(viewsets.ViewSet):
                 'Provides more functionality with less code',
             ]
             return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+        def create(self, request):
+
+            """Create a new hello message."""
+            serializer = self.serializer_class(data=request.data)
+
+            if serializer.is_valid():
+                name = serializer.validated_data.get('name')
+                message = f'Hello {name}!'
+            
+                return Response({'message': message})
+        
+            else:
+                return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        def retrieve(self, request, pk=None):
+
+            """Handle getting an object by its ID"""
+
+            return Response({'http_method': 'GET'})
+
+        def update(self, request, pk=None):
+            """Handle updating an object"""
+
+            return Response({'http_method': 'PUT'})
+
+        def partial_update(self, request, pk=None):
+            """Handle updating part of an object"""
+
+            return Response({'http_method': 'PATCH'})
+
+        def destroy(self, request, pk=None):
+            """Handle removing an object"""
+
+            return Response({'http_method': 'DELETE'}) 
+
+
+...
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all() # this tell the drf to use all the objects of models
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+
+    '''The obtain_auth_token view will return a JSON response when valid username and password fields are POSTed to the view using form data or JSON:'''
+
+    
+    '''No router for this it is not inherited from viewset'''
+
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    '''we are this for better admin presentation in admin as rest of the
+    the classes have it by default.'''
+
