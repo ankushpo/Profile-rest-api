dfc848d028b48722dc9b649c84d9dc3732f5f1c1
AnkushP <ankushpokalwar777@gmail.com>
2020-03-08 22:57:28 +0530

    token added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from profiles_api import permissions
 from profiles_api import serializers
